369609f3049d0bab0cda9229e419a995e302c7e0
Diadlo <polsha3@gmail.com>
2016-11-27 14:15:53 +0300

    Added function

--- 3.s
+++ 3.s
@@ -10,7 +10,6 @@ space:
 zero:
     .string "0"
 
-
 .bss
 text:
     .space 100
@@ -48,46 +47,54 @@ EXIT:
     xor     %rdi, %rdi   # Код возврата 0
     syscall
 
+CHECK_TEXT:
+    mov   %rsp, %rbp
+
+    lea   8(%rbp), %rax 
+    mov   (%rax), %r10  # rax = text length
+    lea   16(%rbp), %rax
+    mov   (%rax), %rdx  # rdx = text
+
+    xor   %rax, %rax
+    movw  space, %ax    # rax == ' '
+    xor   $-1, %rdx     # rdx = -rdx
+    xor   %r13, %r13    # r13 = 0 // count
+
+    START:
+        movb  (%rdi), %bl   # bl = first char
+        repne scasb 
+        cmp   (%rdi), %bl   # new first char == prev first char
+        jne NOT_EQ
+        inc   %r13
+        
+    NOT_EQ:
+        lea   (%rdx, %rdi), %r11  # rcx = offset
+        cmp   %r10, %r11  # offset < length
+        jl    START
+
+    mov  %r13, %rax
+    ret
+
+    
 _start:
     lea   text, %rdi
     push  %rdi
     call  GET_TEXT    # rax = text size
 
-    mov   %rax, %r10  # r10 = rax = text length
-    movw  space, %ax  # rax == ' '
-    lea   text, %rdx  # rdx = text
-    xor   $-1, %rdx
-
-    xor   %r13, %r13  # r13 == count
-
-START:
-    movb  (%rdi), %bl   # bl = first char
-    repne scasb 
-    cmp   (%rdi), %bl   # new first char == prev first char
-    jne NOT_EQ
-    inc   %r13
+    push  %rax        # text still in stack
+    call  CHECK_TEXT  # rax = answer
+    add   $16, %rsp   # pop; pop;
     
-NOT_EQ:
-    lea   (%rdx, %rdi), %r11  # rcx = offset
-    cmp   %r10, %r11  # offset < length
-    jl    START
-
-    lea   zero, %rax
-    xor   %rbx, %rbx
-    movb  (%rax), %bl
-    add   %rbx, %r13
+    movb  (zero), %bl # rbx = '0'
+    add   %bl, %al    # rbx += answer
 
     sub   $1, %rsp
-    mov   %r13, %rax
     movb  %al, (%rsp)
-
-    sub   $1, %rsp
-    movb  $10, (%rsp)
 
     mov   $1, %rax    # write
     mov   $1, %rdi    # файловый дескриптор (1 == stdout)
     mov   %rsp, %rsi  # buf = rsp
-    mov   $2, %rdx    # count = 2
+    mov   $1, %rdx    # count = 2
     syscall
 
     call EXIT
